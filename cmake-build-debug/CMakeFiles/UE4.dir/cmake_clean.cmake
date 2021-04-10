8900ab6c5ca6b6f598e23b24f735fb2a8f0e67da
Jasmine <szhan005@odu.edu>
2021-04-10 02:00:50 -0400

    Sec 2

---
+++ cmake-build-debug/CMakeFiles/UE4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UE4.dir/TripleX.cpp.o"
+  "UE4"
+  "UE4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UE4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
